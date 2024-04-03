457e873de32b20cbd594e5691a6970a8ea9bbb7a
Junior Paradelo <juniorparadelo@gmail.com>
2024-04-03 18:47:19 +0200

    fix: correct compile 03-numbers

--- src/03-numbers.ts
+++ src/03-numbers.ts
@@ -7,13 +7,13 @@
   //customerAge = customerAge + '1';
   customerAge = customerAge + 1;
   console.log('customerAge: ', customerAge);
-
+/*
   let productInStock: number;
   console.log('productInStock: ', productInStock);
   if (productInStock > 10) {
     console.log('is greater!');
   }
-
+*/
   // NaN in runtime
   let discount = parseInt('N');
   console.log('discount: ', discount);
